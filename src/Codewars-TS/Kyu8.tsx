270399d8872139f0b895a536443da275215e5987
Aleksei Pronin <91370169+AlekseiPronin313@users.noreply.github.com>
2023-04-15 14:03:26 +0500

    Test

--- src/Codewars-TS/Kyu8.tsx
+++ src/Codewars-TS/Kyu8.tsx
@@ -7,6 +7,10 @@ import React from "react";
 // console.log(parseF('1'))
 // console.log(parseF('true'))
 
+// const twoSort = (arr: string[]): string => arr.sort()[0].split('').join('***');
+// console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
+// console.log(twoSort(["i", "want", "to", "travel", "the", "world", "writing", "code", "one", "day"]))
+
 const Kyu8 = () => (
     <div>8kyu</div>
 )
