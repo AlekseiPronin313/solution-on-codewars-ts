bfef786d342aa6c578a118cf485f7e35858a70ea
Aleksei Pronin <91370169+AlekseiPronin313@users.noreply.github.com>
2023-04-16 12:43:00 +0500

    Test

---
+++ src/Codewars-TS/Kyu7.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// const sumCubes = (n: number): number => Array.from({length: n}, (_, i) => (i + 1) ** 3).reduce((a, b) => a + b)
+// console.log(sumCubes(1))
+// console.log(sumCubes(2))
+// console.log(sumCubes(3))
+
+const Kyu7 = () => (
+    <div>7kyu</div>
+)
+export default Kyu7
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Kyu8 from "@/Codewars-TS/Kyu8";
 import React from "react";
+import Kyu7 from "@/Codewars-TS/Kyu7";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Kyu8/>
+    <main className="flex min-h-screen flex-col items-center p-24">
+        <Kyu7/>
+        <Kyu8/>
     </main>
   )
 }
