6fd8aff6070a31b1bddd5a3d67f7e6a659400f08
Aleksei Pronin <91370169+AlekseiPronin313@users.noreply.github.com>
2023-04-30 13:19:07 +0500

    Test

--- src/Codewars-TS/Kyu7.tsx
+++ src/Codewars-TS/Kyu7.tsx
@@ -5,6 +5,12 @@ import React from "react";
 // console.log(sumCubes(2))
 // console.log(sumCubes(3))
 
+
+// const removeFire = (str: string): string => str.replace(/Fire/g, '~~')
+// console.log(removeFire('Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast'))
+// console.log(removeFire('Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain'))
+
+
 const Kyu7 = () => (
     <div>7kyu</div>
 )
